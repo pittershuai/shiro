cdf60e3330d65e2946684819f1b8729a13ee16ca
m15650181192@163.com <123>
2019-05-13 21:20:08 +0800

    shiro 认证

--- shiro-test/src/test/java/com.bupt.test/AuthenticationTest.java
+++ shiro-test/src/test/java/com.bupt.test/AuthenticationTest.java
@@ -1,5 +1,36 @@
-package java.com.bupt.test;
+package com.bupt.test;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.subject.Subject;
+import org.junit.Before;
+import org.junit.Test;
 
 public class AuthenticationTest {
+    //使用简单的realm
+    SimpleAccountRealm simpleAccountRealm = new SimpleAccountRealm();
 
+    @Before
+    public void addUser(){
+        simpleAccountRealm.addAccount("tom","123");
+    }
+
+    @Test
+    public void testAuthentication(){
+        //    1、构建SecurityManager环境
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+        defaultSecurityManager.setRealm(simpleAccountRealm);
+
+        //主体提交认证请求
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+        Subject subject = SecurityUtils.getSubject(); //获得主体
+
+        UsernamePasswordToken token = new UsernamePasswordToken("tom", "123");
+        subject.login(token);
+        System.out.println("isAuthenticated:"+ subject.isAuthenticated());
+        subject.logout();
+        System.out.println("isAuthenticated:"+ subject.isAuthenticated());
+    }
 }
